61922aaf7075ed0fec1070134f513b2662a931e5
Jack Huang <jackhuang@jackde-macbook-air.local>
2018-07-23 12:14:00 +0800

    test

--- groot speaker/ViewController.swift
+++ groot speaker/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lovetextfield: UITextField!
+    @IBAction func speak(_ sender: UIButton) {
+        let speechUtterance = AVSpeechUtterance(string: lovetextfield.text! )
+        
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(speechUtterance)
+    }
+    
+    var myPlayer:AVAudioPlayer?
+    @IBAction func playSound(_ sender: UIButton) {
+        myPlayer?.stop()
+        myPlayer?.currentTime = 0.0
+        myPlayer?.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let Path = Bundle.main.path(forResource: "iamgroot/Users/jackhuang/Desktop/iamgroot.m4a", ofType: "m4a"){
+            do {
+                myPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: Path))
+            } catch {
+                myPlayer = nil
+            }
+            
+        }
+        
     }
-
-
+    
 }
 
